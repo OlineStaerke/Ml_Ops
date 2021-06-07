515d49ce37b223ec75c9265b6fc30d48340e31cf
osta0005 <olinestaerke@hotmail.com>
2021-06-07 14:56:08 +0200

    with cookiecutter

---
+++ src/models/predict_model.py
@@ -0,0 +1,93 @@
+import sys, os
+import argparse
+
+import torch
+from model import MyAwesomeModel
+from torch import nn, optim
+import matplotlib.pyplot as plt
+from PIL import Image
+from torchvision import datasets, transforms
+import helper
+
+
+class Predict(object):
+    """ Helper class that will help launch class methods as commands
+        from a single script
+    """
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Script for either training or evaluating",
+            usage="python main.py <command>"
+        )
+        parser.add_argument("command", help="Subcommand to run")
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print('Unrecognized command')
+            
+            parser.print_help()
+            exit(1)
+        # use dispatch pattern to invoke method with same name
+        getattr(self, args.command)()
+    
+
+    def predict(self):
+        transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                                ])
+
+
+        #Change directory
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        os.chdir(dir_path)
+
+        model = MyAwesomeModel()
+        print("Predict until hitting the ceiling")
+        parser = argparse.ArgumentParser(description='Training arguments')
+        parser.add_argument('--load_model_from', default="")
+        parser.add_argument('--load_data_from', default="")
+        
+        args = parser.parse_args(sys.argv[2:])
+        print(args)
+       
+        #Load model in to system
+        if args.load_model_from:
+            state_dict = torch.load(args.load_model_from)
+            model.load_state_dict(state_dict)
+        
+        #Load data in to system
+        if args.load_data_from:
+        
+            img = Image.open(args.load_data_from)
+        
+        else:
+            img = Image.open('../../data/raw/00000.jpg')
+            
+        # Transform
+        img = transform(img)
+        model.eval()
+
+
+        # Turn off gradients to speed up this part
+        with torch.no_grad():
+            logps = model(img)
+
+        # Output of the network are log-probabilities, need to take exponential for probabilities
+        ps = torch.exp(logps)
+        print(ps)
+        helper.view_classify(img.view(1, 28, 28), ps)
+        plt.show()
+        
+      
+
+
+if __name__ == '__main__':
+    Predict()
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ src/visualization/visualize.py
@@ -0,0 +1,99 @@
+import sys, os
+import argparse
+import torch
+sys.path.append('../')
+from models.model import MyAwesomeModel
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.manifold import TSNE
+
+class Visualize(object):
+    """ Helper class that will help launch class methods as commands
+        from a single script
+    """
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Script for either training or evaluating",
+            usage="python main.py <command>"
+        )
+        parser.add_argument("command", help="Subcommand to run")
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print('Unrecognized command')
+            
+            parser.print_help()
+            exit(1)
+        # use dispatch pattern to invoke method with same name
+        getattr(self, args.command)()
+    
+
+    def visualize(self):
+        #Change directory
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        os.chdir(dir_path)
+
+        model = MyAwesomeModel()
+
+        parser = argparse.ArgumentParser(description='Training arguments')
+        parser.add_argument('--load_model_from', default="")
+        parser.add_argument('--load_data_from', default="")
+
+        args = parser.parse_args(sys.argv[2:])
+       
+       
+        #Load model in to system
+        if args.load_model_from:
+            state_dict = torch.load(args.load_model_from)
+            model.load_state_dict(state_dict)
+        else:
+            state_dict = torch.load('../../models/model.pth')
+            model.load_state_dict(state_dict)
+
+        #Load data in to system
+        if args.load_data_from:
+            test_set = torch.load(args.load_data_from)
+        
+        else:
+            test_set = torch.load('../../data/processed/test.pt')
+        
+
+    
+        predictions = torch.empty(64, 64)
+        pred_labels = [0]*64
+     
+        with torch.no_grad():    
+            model.eval()
+            for images, labels in test_set:
+                log_ps = model(images,return_f=True)
+                s = log_ps.shape
+                if(s[0]==64):
+                    predictions = torch.cat([predictions,log_ps], 0)
+                    pred_labels+=labels.tolist()
+        
+        print(pred_labels)
+        x = np.asarray(TSNE(n_components=2).fit_transform(predictions))
+
+        plt.scatter(
+            x=x[:,0],
+            y=x[:,1],
+            c=pred_labels,
+            
+        )
+        plt.show()
+                
+
+    
+
+
+                
+
+         
+                
+            
+            
+        
+
+
+if __name__ == '__main__':
+    Visualize()
+    
